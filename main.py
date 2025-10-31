2f21e5a79a0ae7ae1d516d5a83b03669742ab647
Enzo Pirineli Pacheco <39834559+pirineli@users.noreply.github.com>
2025-10-31 08:37:11 -0300

    fix: metofo head permitido

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from datetime import datetime, timedelta, timezone
 
+
 app = FastAPI()
+
 
 fuso_sp = timezone(timedelta(hours=-3))
 
-@app.get("/")
+@app.api_route("/", methods=["GET", "HEAD"])
 def read_root():
     return {"message": "Server is running!"}
+
 
 @app.get("/now")
 async def agora():
